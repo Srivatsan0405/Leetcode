0339858386d248e62bfbbe789f64d3fa6bcfb409
SRIVATSAN V <139841630+Srivatsan0405@users.noreply.github.com>
2025-08-07 19:50:21 +0530

    Time: 7 ms (1.55%), Space: 18.1 MB (25.04%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        num_to_word_map = {
+            1000000000: "Billion", 1000000: "Million", 1000: "Thousand", 100: "Hundred",
+            90: "Ninety", 80: "Eighty", 70: "Seventy", 60: "Sixty", 50: "Fifty", 40: "Forty",
+            30: "Thirty", 20: "Twenty", 19: "Nineteen", 18: "Eighteen", 17: "Seventeen",
+            16: "Sixteen", 15: "Fifteen", 14: "Fourteen", 13: "Thirteen", 12: "Twelve",
+            11: "Eleven", 10: "Ten", 9: "Nine", 8: "Eight", 7: "Seven", 6: "Six", 5: "Five",
+            4: "Four", 3: "Three", 2: "Two", 1: "One"
+        }
+
+        if num == 0:
+            return "Zero"
+
+        def helper(n):
+            result = []
+            for value in sorted(num_to_word_map.keys(), reverse=True):
+                while n >= value:
+                    if value >= 100:
+                        result.append(helper(n // value))
+                        result.append(num_to_word_map[value])
+                        n %= value
+                    else:
+                        result.append(num_to_word_map[value])
+                        n -= value
+            return ' '.join(result)
+
+        return helper(num).strip()
